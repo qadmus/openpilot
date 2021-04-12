b411eef1ab74a5c7ad1885ae0baaabd9ea74e07f
qadmus <42746943+qadmus@users.noreply.github.com>
2021-04-11 22:18:54 -0700

    GM minimum engage speed 3 mph

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -42,7 +42,7 @@ class CarInterface(CarInterfaceBase):
 
     if candidate == CAR.VOLT:
       # supports stop and go, but initial engage must be above 18mph (which include conservatism)
-      ret.minEnableSpeed = 18 * CV.MPH_TO_MS
+      ret.minEnableSpeed = 3 * CV.MPH_TO_MS
       ret.mass = 1607. + STD_CARGO_KG
       ret.wheelbase = 2.69
       ret.steerRatio = 15.7
